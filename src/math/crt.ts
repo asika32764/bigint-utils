109946438d9d52662e9724899518ce659bba95bb
Simon Asika <asika32764@gmail.com>
2023-11-15 21:04:08 +0800

    Use native n syntax

--- src/math/crt.ts
+++ src/math/crt.ts
@@ -4,8 +4,10 @@ import { modInv } from './modInv';
  * Function to implement Chinese Remainder Theorem.
  */
 export function crt(num: bigint[], rem: bigint[]): bigint {
-  let sum = BigInt(0);
-  const prod = num.reduce((acc, val) => acc * val, BigInt(1)); // Product of all numbers
+  let sum = 0n;
+
+  // Product of all numbers
+  const prod = num.reduce((acc, val) => acc * val, 1n);
 
   for (let i = 0; i < num.length; i++) {
     const p = prod / num[i];
